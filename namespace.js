c37a54eda4c5eb97dbec80851e77820bc65c6057
JS50 <sant0178@algonquinlive.com>
2017-10-18 17:21:46 -0400

    yuh

---
+++ namespace.js
@@ -0,0 +1,22 @@
+var SANT0178{
+    
+    init: function () {
+        var div = document.createElement("div");
+        div.className = "box";
+        div.textContent = "SANT0178";
+        document.getElementById("boxes").appendChild(div);
+
+        div.addEventListener("click", yes);
+        div.addEventListener("mouseover", mouseListen);
+        div.addEventListener("mouseout", mouseListen);
+
+        function yes(ev) {
+            this.style.backgroundColor = "salmon";
+            this.style.borderColor = "yellow";
+        }
+
+        function mouseListen(ev) {
+            ev.currentTarget.classList.toggle("highlight");
+        }
+    }
+}
